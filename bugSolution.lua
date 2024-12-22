4f291eeda7cb8eb96898467fad9e12283a063c81
BugHunter-X <askzenai@gmail.com>
2024-12-22 09:46:06 +0100

    Add bugSolution.lua file

---
+++ bugSolution.lua
@@ -0,0 +1,19 @@
+local function foo(t)
+  local visited = {}
+  local function traverse(t)
+    for k, v in pairs(t) do
+      if type(v) == "table" then
+        if not visited[v] then
+          visited[v] = true
+          traverse(v)
+        end
+      end
+    end
+  end
+  traverse(t)
+  return t
+end
+
+local t = {a = 1, b = {c = 2, d = {e = 3}}}
+t = foo(t)
+print(t.b.d.e)
